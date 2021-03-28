053d23a13fcb2a224f284fd1898a548f67dd07f4
Sumannagella79 <81511414+Sumannagella79@users.noreply.github.com>
2021-03-28 11:40:17 +0100

    Create PasswordValidation.java
    validating username and password from the text file

---
+++ PasswordValidation.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class FileReading {
+    public static void main(String args[]) throws FileNotFoundException{
+      Scanner fileScan = new Scanner (new File("test.txt"));
+      boolean found = false; // added this variable
+      while (fileScan.hasNextLine()) {
+        String input = fileScan.nextLine();
+        String Username = input.substring(0,input.indexOf(' '));
+        String Password = input.substring(input.indexOf(' ')+1,input.length());
+
+
+        if ((Username.equals("pawan")) && (Password.equals("kalyan"))) {
+          found = true; // added this to set found
+          System.out.println("Success");
+
+        } 
+      }
+
+      if(!found) { 
+        System.out.println("failed");
+      }
+    }
+}
